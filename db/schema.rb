6064a0c65391e386bc8affec9d0ea7d80df7ce7c
Haess Romani <hromaza@gmail.com>
2024-03-20 10:41:48 +0100

    Discussion model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_19_151541) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_20_093751) do
+  create_table "discussions", force: :cascade do |t|
+    t.string "name"
+    t.boolean "pinned", default: false
+    t.boolean "closed", default: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_discussions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_19_151541) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "discussions", "users"
 end
