84ecc30bf86a94145e76cb353e6e62aa5365cca9
Haess Romani <hromaza@gmail.com>
2024-03-24 19:31:28 +0100

    Create discussion from the UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :discussions, only: [:index]
+  resources :discussions, only: %i[index create new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
